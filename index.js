7062732f8ebcc109d940a17cf476ef639f1266c5
Amenov <amenov.abdulsalam@gmail.com>
2021-01-17 15:52:14 +0600

    M 1552 17012021

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const Validator = require('amenov.validator');
 
-module.exports = ({ sequelize }) => (req, res, next) => {
+module.exports = (options) => (req, res, next) => {
   req.validation = async (rules) => {
-    const validation = new Validator(req.body, rules, { sequelize });
+    const validation = new Validator(req.body, rules, options);
 
     await validation.fails();
 
